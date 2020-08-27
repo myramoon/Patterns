c1553621e237c829591368fabf5cfdc89cabc15f
anamfazal <anamfazal94@gmail.com>
2020-08-27 18:01:43 +0530

    ADDED:Validation pattern for email

--- user_Registration.sh
+++ user_Registration.sh
@@ -1,10 +1,13 @@
-#!/usr/local/bin/bash
+#!/usr/local/bin/bash -x
 
-read -p "Enter your first and last name " input
+read -p "Enter your first and last name " input1
+read -p "Enter your email: " input2
 
 name="^[A-Z]{1}[a-z]{2,}[[:space:]]{1}[A-Z]{1}[a-z]{2,}$"
 
-if [[ $input =~ $name ]]
+email="^[A-Za-z0-9]+([-\.\+\_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([\.\,]{1}[a-z]{2,3}){1}$"
+
+if [[ $input1 =~ $name && $input2 =~ $email ]]
 then
         echo "valid"
 else
