5cbadbc098890a0f72e3d4fbf6f1c18f5abce390
anamfazal <anamfazal94@gmail.com>
2020-08-27 18:15:28 +0530

    ADDED:Validation pattern1 for password

--- user_Registration.sh
+++ user_Registration.sh
@@ -3,17 +3,34 @@
 read -p "Enter your first and last name " input1
 read -p "Enter your email: " input2
 read -p "Enter your mobile number: " input3
+read -p "Enter a password [minimum 8 characters] : " input4
 
 name="^[A-Z]{1}[a-z]{2,}[[:space:]]{1}[A-Z]{1}[a-z]{2,}$"
 email="^[A-Za-z0-9]+([-\.\+\_]{1}[0-9A-Za-z]+)*@[A-Za-z0-9]+.[a-zA-Z]{2,4}([\.\,]{1}[a-z]{2,3}){1}$"
 mobile="^([1-9][0-9]){1}[[:space:]]{1}[1-9]{1}[0-9]{9}$"
+password="^[0-9A-Za-z]{8,}$"
 
-if [[ $input1 =~ $name && $input2 =~ $email && $input3 =~ $mobile ]]
+
+if [[ $input1 =~ $name && $input2 =~ $email && $input3 =~ $mobile && $input4 =~ $password ]]
 then
         echo "valid"
 else
         echo "invalid"
 fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
